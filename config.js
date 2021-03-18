222ba0dee4bcf7036f20222aa2cb431a62424d4a
Narendra Mohan <narendramohan8272@gmail.com>
2021-03-18 17:27:35 +0530

    O auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey" : "12345-67890-09876-54321",
-    "mongoUrl" : "mongodb://localhost:27017/conFusion"
+    "mongoUrl" : "mongodb://localhost:27017/conFusion",
+    "facebook" : {
+        clientId : "431553694740521",
+        clientSecret : "3986f2519a7b4d48e5cad97267c95adc"
+    }
 }
